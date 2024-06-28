17b0a9691f2232e7e7e36e9af8683869e1361062
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-06-28 22:15:26 +0300

    Styles packages installation, Tailwindcss setup, base styles and components setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.{html,js}", "./resources/**/*.blade.php"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // CSS
+                "resources/css/tailwind.css",
+                "resources/css/fontawesome.css",
+                // SCSS
+                "resources/scss/bootstrap.scss",
+                // Javascript
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
+    corePlugins: {
+        preflight: false,
+    },
 });
