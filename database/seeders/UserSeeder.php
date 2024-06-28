d1803f5b44a8f42cf7b627fe26bf8f1d5e45cf91
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-06-28 13:56:17 +0300

    Users migration, seeder, model

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Super admin
+            [
+                'email' => 'super-admin@hello-asso.com',
+                'password' => Hash::make('password'),
+                'role' => 'SUPER_ADMIN'
+            ],
+            // Admin
+            [
+                'email' => 'sitrakaramanoelina@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'ADMIN',
+            ],
+            // Basic / Members
+            [
+                'email' => 'fenitraandrianiaina@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'BASIC',
+            ],
+            [
+                'email' => 'tolotrarabefaly@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'BASIC',
+            ],
+        ]);
+    }
+}
