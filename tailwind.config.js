7419eacfb22ccb7fdb55bd0bc246c9167e94f157
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-08-15 23:45:44 +0300

    Landing page html-css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./resources/**/*.{html,js}", "./resources/**/*.blade.php"],
     theme: {
-        colors: {
-            primary: "#283179",
-            secondary: "#efc600",
-            black: "#000000",
+        extend: {
+            colors: {
+                primary: "#283179",
+                secondary: "#efc600",
+            },
         },
-        extend: {},
     },
     plugins: [],
 };
