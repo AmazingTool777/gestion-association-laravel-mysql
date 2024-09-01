42a42b67b5722913241ee606b1eaeb93e96982d0
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-09-01 20:55:29 +0300

    Fix of donation calls table migration

--- database/migrations/2024_06_28_133818_create_donation_calls_table.php
+++ database/migrations/2024_06_28_133818_create_donation_calls_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('donation_calls', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->string("type");
             $table->text("description");
             $table->unsignedBigInteger("collected_amount")->default(0);
             $table->unsignedBigInteger("required_amount");
